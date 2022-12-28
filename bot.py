973c887014362b46a81c242c3afd5df8c5b58843
megahomyak <g.megahomyak@gmail.com>
2022-12-28 12:42:48 +0500

    Added timestamps to the logger

--- bot.py
+++ bot.py
@@ -98,7 +98,7 @@ dp = Dispatcher(bot)
 logger = logging.getLogger("hoarder")
 logger.setLevel(logging.DEBUG)
 stream_handler = logging.StreamHandler()
-formatter = logging.Formatter("%(levelname)s:%(name)s:%(message)s")
+formatter = logging.Formatter("%(asctime)s:%(levelname)s:%(name)s:%(message)s")
 stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
 
